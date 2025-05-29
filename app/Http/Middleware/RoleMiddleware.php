195413e83bf15b8cdff10d61a36c8826f2c6dcee
Михаил Казьмин <mihail.kazmin.04@mail.ru>
2025-05-29 10:53:09 +0500

    Feat: добавлен миддлваре

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  mixed  ...$roles
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        $user = Auth::user();
+
+        if (!$user->hasRole($roles)) {
+            abort(403, 'Доступ запрещен');
+        }
+
+        return $next($request);
+    }
+}
